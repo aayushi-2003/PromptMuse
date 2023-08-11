a84b578e0b9bfccd30c07353278c620a1a926d65
Aayushi Sinha <aayushi.sinha1980@gmail.com>
2023-08-11 20:42:33 +0530

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import '@styles/global.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata={
+    title: "PromptMuse",
+    description: 'Discover and Share AI Prompts'
+
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+            <main className="app">
+                <Nav />
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout;
